ee4e47306645cee5ff2259053bcd912931468c54
vishalchitra <46961746+vishalchitra@users.noreply.github.com>
2022-07-11 14:38:37 +0530

    Create LastDigitTest.java

---
+++ module-numbers/src/test/java/io/codejournal/maven/jacoco/numbers/LastDigitTest.java
@@ -0,0 +1,21 @@
+package io.codejournal.maven.jacoco.numbers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LastDigitTest {
+
+    private LastDigit fixture;
+
+    @BeforeEach
+    public void setUp() {
+        fixture = new LastDigit();
+    }
+
+    @Test
+    public void testcase1() {
+        // Test code here
+    }
+}
