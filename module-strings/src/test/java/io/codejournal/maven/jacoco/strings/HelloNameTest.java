c077c92a9539243a3725d46cf2fbb40f8dd7b56f
vishalchitra <46961746+vishalchitra@users.noreply.github.com>
2022-07-11 14:42:02 +0530

    Create HelloNameTest.java

---
+++ module-strings/src/test/java/io/codejournal/maven/jacoco/strings/HelloNameTest.java
@@ -0,0 +1,21 @@
+package io.codejournal.maven.jacoco.strings;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HelloNameTest {
+
+    private HelloName fixture;
+
+    @BeforeEach
+    public void setUp() {
+        fixture = new HelloName();
+    }
+
+    @Test
+    public void testcase() {
+        // Test code here
+    }
+}
