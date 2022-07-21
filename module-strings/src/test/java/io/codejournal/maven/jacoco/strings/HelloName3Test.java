59167eb186048cd5560a7425e1bf4d451c3f7b75
vishalchitra <46961746+vishalchitra@users.noreply.github.com>
2022-07-21 20:35:54 +0530

    Create HelloName3Test.java

---
+++ module-strings/src/test/java/io/codejournal/maven/jacoco/strings/HelloName3Test.java
@@ -0,0 +1,21 @@
+package io.codejournal.maven.jacoco.strings;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HelloName3Test {
+
+    private HelloName3 fixture;
+
+    @BeforeEach
+    public void setUp() {
+        fixture = new HelloName3();
+    }
+
+    @Test
+    public void testcase() {
+        // Test code here
+    }
+}
