707b7568d49568830ed7a9b8205a12bed8f9815d
vishalchitra <46961746+vishalchitra@users.noreply.github.com>
2022-07-21 20:28:04 +0530

    Create FirstDigitTest.java

---
+++ module-numbers/src/test/java/io/codejournal/maven/jacoco/numbers/FirstDigitTest.java
@@ -0,0 +1,21 @@
+package io.codejournal.maven.jacoco.numbers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FirstDigitTest {
+
+    private FirstDigit fixture;
+
+    @BeforeEach
+    public void setUp() {
+        fixture = new FirstDigit();
+    }
+
+    @Test
+    public void testcase1() {
+        // Test code here
+    }
+}
